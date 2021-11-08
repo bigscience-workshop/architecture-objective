81316477f21cddf21f8bb3993b8345e6b7687c23
thomasw21 <24695242+thomasw21@users.noreply.github.com>
2021-11-08 19:37:55 +0100

    Choose machine type

--- t5x/configs/dataset/pile/run_cache_tasks_main.sh
+++ t5x/configs/dataset/pile/run_cache_tasks_main.sh
@@ -13,4 +13,4 @@ seqio_cache_tasks \
  --module_import=$MODULE_IMPORT \
  --tasks=${TASK_NAME} \
  --output_cache_dir=${BUCKET}/cache \
- --pipeline_options="--runner=DataflowRunner,--project=$PROJECT,--region=$REGION,--job_name=$JOB_NAME,--staging_location=$BUCKET/binaries,--temp_location=$BUCKET/tmp,--setup_file=$PWD/setup.py,--num_workers=32,--autoscaling_algorithm=NONE"
+ --pipeline_options="--runner=DataflowRunner,--project=$PROJECT,--region=$REGION,--job_name=$JOB_NAME,--staging_location=$BUCKET/binaries,--temp_location=$BUCKET/tmp,--setup_file=$PWD/setup.py,--num_workers=32,--autoscaling_algorithm=NONE,--machine_type=n1-highmem-2"
