3e294d94105f5bc881133290c8b045100113799d
thomasw21 <24695242+thomasw21@users.noreply.github.com>
2021-11-08 15:05:00 +0100

    test something else:

--- t5x/configs/dataset/pile/run_cache_tasks_main.sh
+++ t5x/configs/dataset/pile/run_cache_tasks_main.sh
@@ -13,5 +13,4 @@ seqio_cache_tasks \
  --module_import=$MODULE_IMPORT \
  --tasks=${TASK_NAME} \
  --output_cache_dir=${BUCKET}/cache \
- --setup_file t5x/configs/dataset/pile/dataflow_setup.py \
- --pipeline_options="--runner=DataflowRunner,--project=$PROJECT,--region=$REGION,--job_name=$JOB_NAME,--staging_location=$BUCKET/binaries,--temp_location=$BUCKET/tmp"
+ --pipeline_options="--runner=DataflowRunner,--project=$PROJECT,--region=$REGION,--job_name=$JOB_NAME,--staging_location=$BUCKET/binaries,--temp_location=$BUCKET/tmp,--setup_file t5x/configs/dataset/pile/dataflow_setup.py"
