c61071662797ed93d61d484cb203c978799a4174
thomasw21 <24695242+thomasw21@users.noreply.github.com>
2021-12-08 18:40:43 +0100

    WIP

--- bigscience/scripts/lm_adapt.sh
+++ bigscience/scripts/lm_adapt.sh
@@ -41,9 +41,9 @@ fi
 
 
 python3 ${T5X_DIR}/t5x/train.py \
-  --gin_file="bigscience/gins/$GIN_FILE" \
   --gin.INITIAL_CHECKPOINT_PATH="'${CHECKPOINT_DIR}'" \
+  --gin_file="bigscience/gins/$GIN_FILE" \
   --gin.MODEL_DIR="'${MODEL_DIR}'" \
   2>&1 | tee $LOGS_PATH/pretrain_$EXPERIMENT_NAME.txt
 
-# sh bigscience/scripts/t0_adapt.sh c_dec_c4_full_lm 420000
+# sh bigscience/scripts/t0_adapt.sh c_dec_c4_span_corruption_bs_128 420000
