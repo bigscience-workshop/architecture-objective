06556feba7aac21a761ca14731a2c5cadadfc9e6
Lintang Sutawika <lintangsutawika@gpu-st-p4d-24xlarge-20.hpc-1click-prod450.pcluster>
2022-10-06 09:24:31 +0000

    added test pretraining runs

---
+++ experiments/test-pretrain/tasks.py
@@ -0,0 +1,42 @@
+import functools
+import seqio
+import tensorflow_datasets as tfds
+from t5.evaluation import metrics
+from t5.data import preprocessors
+
+TaskRegistry = seqio.TaskRegistry
+
+vocabulary = seqio.SentencePieceVocabulary(
+    'gs://t5-data/vocabs/cc_all.32000/sentencepiece.model', extra_ids=100)
+output_features = {
+    'inputs': seqio.Feature(vocabulary=vocabulary, add_eos=True, required=False),
+    'targets': seqio.Feature(vocabulary=vocabulary, add_eos=True)
+}
+
+# ==================================== C4 ======================================
+# Final pretraining task used in Raffel et al., 2019.
+TaskRegistry.add(
+    "wikipedia.en_unsupervised",
+    source=seqio.TfdsDataSource(tfds_name="wikipedia/20200301.en:1.0.0"),
+    #source=seqio.TfdsDataSource(tfds_name="c4/en:3.0.1"),
+    #source=seqio.TfdsDataSource(
+    #    tfds_name="c4/en.webtextlike:3.0.1",
+    #    splits={
+    #        "train": "train[:85%]",
+    #        "validation": "train[85%:90%]",
+    #        "test": "validation"
+    #    }),
+    preprocessors=[
+        functools.partial(
+            preprocessors.rekey, key_map={
+                "inputs": None,
+                "targets": "text"
+            }),
+        seqio.preprocessors.tokenize,
+        seqio.CacheDatasetPlaceholder(),
+        preprocessors.span_corruption,
+        seqio.preprocessors.append_eos_after_trim,
+
+    ],
+    output_features=output_features,
+    metric_fns=[])
