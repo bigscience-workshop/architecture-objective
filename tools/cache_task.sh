8b6ce16cfee8773e65e1866f58b3869c5383dd92
haileyschoelkopf <hailey.schoelkopf@yale.edu>
2022-10-06 13:34:41 -0400

    update caching script

--- tools/cache_task.sh
+++ tools/cache_task.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
 
+# change variables as desired
 MODULE=mixtures.c4.span_corruption
 TASK_NAME=c4_eye_span_corruption
-CACHE_DIR=gs://t5x-test/seqio_cached_tasks/
+CACHE_DIR=gs://t5x-test/seqio_cached_tasks
 
 seqio_cache_tasks \
     --module_import=$MODULE \
