3266f5c9d44c7221db22baee4fd18ea11300a7e8
Lintang Sutawika <lintang@datasaur.ai>
2022-10-24 20:35:03 +0700

    Update c4_utils.py
    last two chunks reserved for `val` and `test` split.

--- t5x/data/c4_utils.py
+++ t5x/data/c4_utils.py
@@ -5,7 +5,7 @@ def get_c4_files():
     """name of the c4 files"""
 
     return {
-        "train": [f"c4-train.{i:05}-of-01024.json" for i in range(1024)],
-        "val": [f"c4-train.{i:05}-of-01024.json" for i in range(1024)],
-        "test": [f"c4-train.{i:05}-of-01024.json" for i in range(1024)],
+        "train": [f"c4-train.{i:05}-of-01024.json" for i in range(1022)],
+        "val": [f"c4-train.01022-of-01024.json"],
+        "test": [f"c4-train.01023-of-01024.json"],
         }
