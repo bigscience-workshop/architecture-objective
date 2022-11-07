c1fe809906240284417d87b5e5111bb2146deecb
lintangsutawika <lintang@sutawika.com>
2022-11-07 14:44:17 +0000

    changed back default vocab to the one hosted in gcp

--- t5x/data/vocab.py
+++ t5x/data/vocab.py
@@ -4,11 +4,7 @@
 import seqio
 
 # # TODO: Link to Eleuther's custom default tokenizer when ready.
-# DEFAULT_SPM_PATH = "gs://t5-data/vocabs/cc_all.32000/sentencepiece.model"  # GCS
-# DEFAULT_EXTRA_IDS = 100
-
-DEFAULT_SPM_PATH = "/fsx/lintangsutawika/t5-tokenizer/spiece.model"
-DEFAULT_EXTRA_IDS = 0
+DEFAULT_SPM_PATH = "gs://t5-data/vocabs/cc_all.32000.100extra/sentencepiece.model"  # GCS
 
 def get_default_vocabulary():
-  return seqio.SentencePieceVocabulary(DEFAULT_SPM_PATH, DEFAULT_EXTRA_IDS)
+    return seqio.SentencePieceVocabulary(DEFAULT_SPM_PATH)
